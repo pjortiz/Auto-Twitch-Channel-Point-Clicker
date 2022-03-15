47a57642ede1ad587fff16eb5b2c56a65f2e78c3
Peter O <pjo_6493@hotmail.com>
2022-03-15 19:27:17 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Auto-Twitch-Channel-Point-Clicker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @icon         https://i.imgur.com/Jks7wvm.png
+// @description  Auto Twitch.tv channel point clicker
+// @author       Ortiz, Peter 
+// @match        https://www.twitch.tv/*
+// @updateURL    
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
