e57f8321ab249ba1ef15380495c41d655a5c632b
Peter O <pjo_6493@hotmail.com>
2022-03-15 21:48:58 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Auto-Twitch-Channel-Point-Clicker
-// @namespace    http://tampermonkey.net/
+// @namespace    http://pjortiz.github.io/
 // @version      1
 // @resource     icon https://i.imgur.com/Jks7wvm.png
 // @description  Auto Twitch.tv channel point clicker
 // @author       Ortiz, Peter
 // @match        https://www.twitch.tv/*
-// @website      https://pjortiz.github.io/Auto-Twitch-Channel-Point-Clicker/
 // @updateURL    https://pjortiz.github.io/Auto-Twitch-Channel-Point-Clicker/script.meta.js
 // @downloadURL  https://pjortiz.github.io/Auto-Twitch-Channel-Point-Clicker/script.user.js
 // @run-at       document-end
