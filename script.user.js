6c2163b2c93311eebefabf1900e40046d559796f
Peter O <pjo_6493@hotmail.com>
2022-03-15 20:28:45 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,7 +4,7 @@
 // @version      1
 // @resource     icon https://i.imgur.com/Jks7wvm.png
 // @description  Auto Twitch.tv channel point clicker
-// @author       Ortiz, Peter 
+// @author       Ortiz, Peter
 // @match        https://www.twitch.tv/*
 // @website      https://pjortiz.github.io/Auto-Twitch-Channel-Point-Clicker/
 // @updateURL    https://pjortiz.github.io/Auto-Twitch-Channel-Point-Clicker/script.meta.js
